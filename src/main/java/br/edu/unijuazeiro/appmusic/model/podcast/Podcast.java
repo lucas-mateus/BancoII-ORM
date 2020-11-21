badea4cebf1d2a62ef549749c225496c72d4cbd4
lucas-mateus <xlucasmat@gmail.com>
2020-11-21 03:34:53 -0300

    adicionando gerador automatico e id

--- src/main/java/br/edu/unijuazeiro/appmusic/model/podcast/Podcast.java
+++ src/main/java/br/edu/unijuazeiro/appmusic/model/podcast/Podcast.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -21,6 +23,7 @@ import lombok.NoArgsConstructor;
 public class Podcast {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idPodcast;
     @Column(nullable = false)
     private String owner;
