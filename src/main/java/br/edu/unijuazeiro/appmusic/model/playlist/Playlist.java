102797b99e02242ca5b1e6a00f5613700d09aae8
lucas-mateus <xlucasmat@gmail.com>
2020-11-21 03:34:33 -0300

    adicionando gerador automatico de id

--- src/main/java/br/edu/unijuazeiro/appmusic/model/playlist/Playlist.java
+++ src/main/java/br/edu/unijuazeiro/appmusic/model/playlist/Playlist.java
@@ -1,6 +1,8 @@
 package br.edu.unijuazeiro.appmusic.model.playlist;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,6 +17,7 @@ import lombok.Data;
 public class Playlist {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String namePlaylist;
 
